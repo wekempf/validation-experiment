e6b258169eedd36810c15964d4562c66885f10ea
Bill Kempf <Bill.Kempf@salliemae.com>
2018-08-22 08:32:56 -0400

    Implement simple validation

--- src/cljs/validation_experiment/core.cljs
+++ src/cljs/validation_experiment/core.cljs
@@ -1,46 +1,16 @@
 (ns validation-experiment.core
-    (:require [reagent.core :as reagent :refer [atom]]
-              [secretary.core :as secretary :include-macros true]
-              [accountant.core :as accountant]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [validation-experiment.ssn :as ssn]
+            [clojure.spec.alpha :as s]))
 
-;; -------------------------
-;; Views
+(defonce ssn (atom "123-45-6789"))
 
-(defn home-page []
-  [:div [:h2 "Welcome to validation-experiment"]
-   [:div [:a {:href "/about"} "go to about page"]]])
-
-(defn about-page []
-  [:div [:h2 "About validation-experiment"]
-   [:div [:a {:href "/"} "go to the home page"]]])
-
-;; -------------------------
-;; Routes
-
-(defonce page (atom #'home-page))
-
-(defn current-page []
-  [:div [@page]])
-
-(secretary/defroute "/" []
-  (reset! page #'home-page))
-
-(secretary/defroute "/about" []
-  (reset! page #'about-page))
-
-;; -------------------------
-;; Initialize app
+(defn example []
+  [:div [:label "Enter SSN: " [:input {:type :text :value @ssn :on-change #(reset! ssn (-> % .-target .-value))}]]
+   [:p {:style {:color "red"}} (str (s/explain-data :validation-experiment.ssn/ssn @ssn))]])
 
 (defn mount-root []
-  (reagent/render [current-page] (.getElementById js/document "app")))
+  (reagent/render [example] (.getElementById js/document "app")))
 
 (defn init! []
-  (accountant/configure-navigation!
-    {:nav-handler
-     (fn [path]
-       (secretary/dispatch! path))
-     :path-exists?
-     (fn [path]
-       (secretary/locate-route path))})
-  (accountant/dispatch-current!)
   (mount-root))
